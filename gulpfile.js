693a5fb45f2a2d48f264b83e656820a1c21331aa
fmaylinch <ferranmaylinch@gmail.com>
2016-04-06 11:49:02 +0200

    Support more browsers with autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -10,8 +10,7 @@ gulp.task('css', function () {
 	return gulp.src('css/processed/*.scss')
 
 		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
+			//browsers: ['last 2 versions']
 		}))
 
 		.pipe(sass().on('error', sass.logError))
