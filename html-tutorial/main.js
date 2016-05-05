01b291aacd267076522833f47f09f56f72511ee1
fmaylinch <ferranmaylinch@gmail.com>
2016-05-05 02:02:03 +0200

    Many changes in tutorial (content and style)

--- html-tutorial/main.js
+++ html-tutorial/main.js
@@ -1,9 +1,36 @@
 $(document).ready(function() {  
   
-  // Fix HTML code snippets
-  $('pre code.language-html').each(function(i, block) {
-    var html = Prism.highlight(block.innerHTML, Prism.languages.html);
-    $(block).html(html);
-  });  
+  function getLang(elem) {
+    
+    if (elem.hasClass("language-html")) return Prism.languages.html;
+    if (elem.hasClass("language-css")) return Prism.languages.css;
+    
+    throw "Unexpected language or missing class";
+  }
+  
+  // Adds data-line-offset (http://prismjs.com/plugins/line-highlight/)
+  // according to data-start (http://prismjs.com/plugins/line-numbers/)
+  function setupLineOffset(elem) {
+      
+    if (elem.attr("data-start")) {
+      var start = elem.attr("data-start");
+      elem.attr("data-line-offset", start-1);
+    }
 
+  }
+  
+  // Note: currently html can't contain <body> ?
+  
+  $('pre code.prism').each(function() {
+
+    var parent = $(this).parent();
+    setupLineOffset(parent);
+    
+    var lang = getLang($(this));
+    
+    var html = Prism.highlight($(this).html(), lang);
+    $(this).html(html);
+    
+  });  
+  
 });
