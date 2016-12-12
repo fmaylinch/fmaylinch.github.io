4349e0383cac175e1e8433e5208eec5edb16c8f7
fmaylinch <ferranmaylinch@gmail.com>
2016-12-12 23:48:07 +0100

    Smart house sample

---
+++ files/java/samples/smarthouse/SmartHouseSample.java
@@ -0,0 +1,15 @@
+public class SmartHouseSample {
+
+	public static void main(String[] args) {
+
+		SmartController controller = new SmartController();
+		controller.prepareMovieNight();
+
+		Oven oven = controller.getOven();
+		Tv tv = controller.getTv();
+
+		System.out.println("Oven: " + oven.toString());
+		System.out.println("Tv: " + tv.toString());
+
+	}
+}
