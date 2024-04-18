f5811825c9423534778e2005c82d202b3757d160
Ferran Maylinch <ferran@yandex-team.ru>
2024-04-18 10:54:41 +0300

    simplify layout

--- vite-ts-app/src/main.ts
+++ vite-ts-app/src/main.ts
@@ -5,68 +5,50 @@ import { initTemplate } from "./js/main";
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div>
-        <!-- Sidebar -->
-        <section id="sidebar">
-            <div class="inner">
-                <nav>
-                    <ul>
-                        <li><a id="main-button" href="#main-section">Main</a></li>
-                        <li><a id="play-button" href="#play-section">Play</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </section>
-
-    <!-- Wrapper -->
         <div id="wrapper">
-
-            <!-- Intro -->
-                <section id="main-section" class="wrapper style1 fullscreen fade-up">
-                    <div class="inner">
-                        <div id="main">
-                            <h1 id="title">Hi, I'm <strong>Ferran</strong></h1>
-                            <p id="text1">I love coding and teaching</p>
-                            <p id="text2">Now I work at <a target="_blank" href="https://cloud.yandex.com/en/">Yandex Cloud</a></p>
-                        </div>
+            <section id="main-section" class="wrapper style1 fullscreen fade-up">
+                <div class="inner">
+                    <div id="react-test"></div>
+                    <div id="main">
+                        <h1>Hi, I'm <strong>Ferran</strong></h1>
+                        <p>I love coding and teaching. Now I work at <a target="_blank" href="https://cloud.yandex.com/en/">Yandex Cloud</a></p>
                     </div>
-                </section>
+                </div>
+            </section>
 
             <div id="errors"></div>
 
-            <!-- Three -->
-                <section id="play-section" class="wrapper style3 fade-up">
-                    <div class="inner">
-                        <div>
-                            <ul class="actions">
-                                <li><a href="#" class="button run submit">Run the code below</a></li>
-                            </ul>
-                        </div>
-                        <form method="post" action="#">
-                            <div id="message" style="max-height: 400px; overflow: auto;"></div>
-                        </form>
-                        <div>
-                            <ul class="actions">
-                                <li><a href="#" class="button reset">Reset code</a></li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h3>Social Networks</h3>
-                            <ul class="icons">
-                                <li><a target="_blank" href="https://github.com/fmaylinch" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
-                                <li><a target="_blank" href="https://www.linkedin.com/in/ferranmaylinch/" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
-                                <li><a target="_blank" href="https://www.instagram.com/fmaylinch/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
-                                <li><a target="_blank" href="https://t.me/fmaylinch" class="icon brands fa-telegram"><span class="label">Telegram</span></a></li>
-                                <li><a target="_blank" href="https://www.youtube.com/user/ferranmaylinch" class="icon brands fa-youtube"><span class="label">Youtube</span></a></li>
-                                <li><a target="_blank" href="https://twitter.com/ferranmaylinch" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-                                <li><a target="_blank" href="https://vk.com/fmaylinch" class="icon brands fa-vk"><span class="label">VK</span></a></li>
-                            </ul>
-                        </div>
+            <section id="play-section" class="wrapper style3 fade-up">
+                <div class="inner">
+                    <div>
+                        <ul class="actions">
+                            <li><a href="#" class="button run submit">Run the code below</a></li>
+                        </ul>
                     </div>
-                </section>
-
+                    <form method="post" action="#">
+                        <div id="message" style="max-height: 400px; overflow: auto;"></div>
+                    </form>
+                    <div>
+                        <ul class="actions">
+                            <li><a href="#" class="button reset">Reset code</a></li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3>Social Networks</h3>
+                        <ul class="icons">
+                            <li><a target="_blank" href="https://github.com/fmaylinch" class="icon brands fa-github"><span class="label">GitHub</span></a></li>
+                            <li><a target="_blank" href="https://www.linkedin.com/in/ferranmaylinch/" class="icon brands fa-linkedin-in"><span class="label">LinkedIn</span></a></li>
+                            <li><a target="_blank" href="https://www.instagram.com/fmaylinch/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+                            <li><a target="_blank" href="https://t.me/fmaylinch" class="icon brands fa-telegram"><span class="label">Telegram</span></a></li>
+                            <li><a target="_blank" href="https://www.youtube.com/user/ferranmaylinch" class="icon brands fa-youtube"><span class="label">Youtube</span></a></li>
+                            <li><a target="_blank" href="https://twitter.com/ferranmaylinch" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+                            <li><a target="_blank" href="https://vk.com/fmaylinch" class="icon brands fa-vk"><span class="label">VK</span></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
         </div>
 
-    <!-- Footer -->
         <footer id="footer" class="wrapper style1-alt">
             <div class="inner">
                 <div>
